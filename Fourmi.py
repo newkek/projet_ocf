5ef31c99203520a287f948b33af5468e41c83583
kek <gallardo.kevin0@gmail.com>
2013-03-24 18:40:27 +0100

    Petits changements de clarté de code

--- Fourmi.py
+++ Fourmi.py
@@ -29,6 +29,7 @@ class Fourmi(Point,threading.Thread):
         coords_nid=self.canvas.coords(id_nid)
         self.nid=(coords_nid[0]+10,coords_nid[1]+10)
         self.departcolo=True
+        print(math.degrees(self.teta))
 
     def continuer(self):
         self.pause=False
@@ -50,7 +51,10 @@ class Fourmi(Point,threading.Thread):
         self.pause=True
         
     def prochain_dep(self,curpos):
+        
         four=(curpos[0]+Globals.fourmi_size/2,curpos[1]+Globals.fourmi_size/2)#four sera le centre de la fourmi
+        
+        
         if self.endurance_courante:
             depx=self.mem_x#deplacement avec l'angle choisi
             depy=self.mem_y
@@ -63,10 +67,9 @@ class Fourmi(Point,threading.Thread):
                 self.retour=True
             depx=depx+random.randint(-2,2)
             depy=depy+random.randint(-1,1) 
-            if self.departcolo is True:
-                self.departcolo=False  
+
         else:#MODE RETOUR
-            ###revenir a la colonie
+            ###revenir a la colonie sans pheromone
             if (self.nid[0]<=four[0] and self.nid[1]<=four[1]):
                 depx=-2
                 depy=-2
@@ -79,34 +82,36 @@ class Fourmi(Point,threading.Thread):
             elif(self.nid[0]>four[0] and self.nid[1]>four[1]):
                 depx=2
                 depy=2
-                
+            if self.trouve:#si on a trouve de la bouf on depose des pheromone sur le retour                
+                depx=2*self.mem_x
+                depy=2*self.mem_y
+                if int(four[0])%3==0 or int(four[1])%3==0:
+                    self.canvas.create_rectangle(four[0]-2,four[1]-2,four[0]+2,four[1]+2,fill='indianred3',width=0,tags="pher")
+                if int(four[0])%10==0 or int(four[1])%10==0:
+                    self.redef_depl(four)    
 
             ###
             depx=depx+random.randint(-1,1)
             depy=depy+random.randint(-1,1)
-            if((self.nid[0]-10)<four[0]<(self.nid[0]+10) and (self.nid[1]-10)<four[1]<(self.nid[1]+10)):#Si on est arrivŽ dans la zone de la colonie    
+            
+            
+            if((self.nid[0]-3)<four[0]<(self.nid[0]+3) and (self.nid[1]-3)<four[1]<(self.nid[1]+3)):#Si on est arrivŽ dans la zone de la colonie    
                 self.endurance_courante=self.endurance#mode retour OFF
                 self.teta=random.randint(0,360)#nouvel angle de depart
                 self.teta=(self.teta*3.14)/180
-                self.mem_x=3*math.cos(self.teta)
-                self.mem_y=3*math.sin(self.teta)
+                self.mem_x=2*math.cos(self.teta)
+                self.mem_y=2*math.sin(self.teta)
                 self.trouve=False
-                self.departcolo=True
-            else:
-                self.departcolo=False
-                
-            if self.trouve:#si on a trouve de la bouf on depose des pheromone sur le retour                
-                depx=2*self.mem_x+random.uniform(-0.5,0.5)
-                depy=2*self.mem_y+random.uniform(-0.5,0.5)
-                if int(four[0])%3==0:
-                    self.canvas.create_rectangle(four[0]-2,four[1]-2,four[0]+2,four[1]+2,fill='indianred3',width=0,tags="pher")
-                #print(four[0])
+                  
+                    
         ###    I.  A.  
         closest=self.canvas.find_closest(four[0]+depx,four[1]+depy,halo=3)#recherche de l'objet le plus proche
         tags_closest=self.canvas.itemcget(closest,"tags")
         if tags_closest=="obst":
             depx=-depx#on evite le l'obstacle, en inversant on cree l effet 'zombie'
             depy=-depy
+            
+            
         elif tags_closest=="ress":
             curwidth=float(self.canvas.itemcget(closest,"width"))
             if curwidth>0:#si la ressource n'est pas 'vide' 
@@ -118,37 +123,24 @@ class Fourmi(Point,threading.Thread):
                     self.canvas.tag_raise(closest)
                 self.endurance_courante=0#mode retour ON
                 self.trouve=True
-                B=[self.nid[0],four[1]]
-                AB=B[0]-four[0]
-                BC=B[1]-self.nid[1]
-                tempx=(self.nid[0]-four[0])/abs(self.nid[0]-four[0])
-                self.teta=-math.atan(BC/AB)
-                self.mem_x=tempx*math.cos(self.teta)
-                self.mem_y=tempx*math.sin(self.teta)
-                print(self.mem_x,self.mem_y)
-                depx=self.mem_x
+                self.redef_depl(four)
+                depx=self.mem_x#premier deplacement pour le retour a vol doiseau
                 depy=self.mem_y
+                
+                
         elif tags_closest=="pher":
             pher=self.canvas.coords(closest)
             if self.endurance_courante:#si on est en mode 'decouverte'
-                if (pher[0]<=four[0] and pher[1]<=four[1]):
+                """if (pher[0]<=four[0] and pher[1]<=four[1]):
                     depx=depx-1
-                    depy=depy-2#Comportement comme les chiens, elle va plus vite lorsquelle est sur la piste
-                elif(pher[0]<=four[0] and pher[0]>four[1]):
-                    depx=depx-1
-                    depy=depy+2
                 elif(pher[0]>four[0] and pher[1]<=four[1]):
-                    depx=depx+1
-                    depy=depy-2
-                elif(pher[0]>four[0] and pher[0]>four[1]):
-                    depx=depx+1
-                    depy=depy+2
+                    depx=depx+1"""
                 
-                if (self.departcolo is True):
-                    dist=math.sqrt(math.pow(pher[0]-four[0],2)+math.pow(pher[1]-four[1],2))
-                    #print(dist, pher[0],pher[1],four[0],four[1])
+                depx=3*math.cos(self.teta)
+                depy=2*depy
+
+
             else:
-                
                 """if (pher[0]<=four[0]+depx):
                     depx=depx+random.randint(0,int(dist))
                     pass
@@ -168,9 +160,17 @@ class Fourmi(Point,threading.Thread):
                     self.mem_x=2
                     self.mem_y=2"""
                 pass
-        #print("fin",self.fourm_Id,depx,depy)
         return depx,depy
-
+    
+    def redef_depl(self,four):
+        B=[self.nid[0],four[1]]###On dŽfini l'angle pour le retour a vol doiseau
+        AB=B[0]-four[0]
+        BC=B[1]-self.nid[1]
+        coef=(self.nid[0]-four[0])/abs(self.nid[0]-four[0])
+        self.teta=-math.atan(BC/AB)
+        self.mem_x=coef*math.cos(self.teta)
+        self.mem_y=coef*math.sin(self.teta)
+        print("redef",math.degrees(self.teta))
         
     
     
